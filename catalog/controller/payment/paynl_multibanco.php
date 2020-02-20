745aefea07f1e00d388421c139daa15ada6d74c7
Max Geraci <max@pay.nl>
2020-02-20 14:01:05 +0100

    Add payment methods Multibanco and Payconiq

---
+++ catalog/controller/payment/paynl_multibanco.php
@@ -0,0 +1,10 @@
+<?php
+$dir = dirname(dirname(dirname(dirname(__FILE__))));
+$autoload = $dir.'/Pay/Autoload.php';
+
+require_once $autoload;
+class ControllerPaymentPaynlMultibanco extends Pay_Controller_Payment {
+  protected $_paymentOptionId = 2271;
+  protected $_paymentMethodName = 'paynl_multibanco';
+
+}
---
+++ catalog/model/payment/paynl_payconiq.php
@@ -0,0 +1,12 @@
+<?php
+$dir = dirname(dirname(dirname(dirname(__FILE__))));
+$autoload = $dir.'/Pay/Autoload.php';
+
+require_once $autoload;
+class ModelPaymentPaynlPayconiq extends Pay_Model {
+  protected $_paymentMethodName = 'paynl_payconiq';
+
+  public function getLabel(){
+    return parent::getLabel();
+  }
+}

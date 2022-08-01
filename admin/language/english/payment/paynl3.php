8b3feebc348d6272021bd998e7c5a11ed3ff8c34
Anne <anne@pay.nl>
2022-08-01 09:55:20 +0200

    different logo

--- admin/language/english/payment/paynl3.php
+++ admin/language/english/payment/paynl3.php
@@ -5,8 +5,8 @@ $_['button_save'] = 'Save';
 $_['button_cancel'] = 'Cancel';
 
 $version = '3.4.8';
-$css = 'position: relative;top: -19px;display: inline;left: 10px;';
-$url = '<a href="https://www.pay.nl" target="_blank"><img style="width: 50px;" src="view/image/payment/main_pay_logo.png" alt="PAY." title="PAY."/></a>' .
+$css = 'position: relative;top: -7px;display: inline;left: 10px;';
+$url = '<a href="https://www.pay.nl" target="_blank"><img style="width: 25px;" src="view/image/payment/main_pay_logo.png" alt="PAY." title="PAY."/></a>' .
     '<div style="' . $css . '">Version: ' . $version . '</div>';
 
 
